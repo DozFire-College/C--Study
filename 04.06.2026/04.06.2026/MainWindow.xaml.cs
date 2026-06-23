3bc47126f2350e5d03abe19ecece6cb0de5294cf
DozFire-College <ttylllihka@gmail.com>
2026-06-23 15:03:07 +0300

    04.06.2026

---
+++ 04.06.2026/04.06.2026/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ProgressBarApp
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+        private int currentValue = 0;
+        private const int MaxValue = 100;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); 
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            currentValue++;
+
+  
+            progressBar.Value = currentValue;
+
+            if (currentValue >= MaxValue)
+            {
+                timer.Stop();
+                ShowCongratulations();
+            }
+        }
+
+        private void ShowCongratulations()
+        {
+            congratulationsPanel.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Interval = TimeSpan.FromSeconds(0.01);
+            timer.Tick += Timer_Tick;
+        }
+    }
+}
